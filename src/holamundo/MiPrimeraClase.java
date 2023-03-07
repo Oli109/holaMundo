8b7f03439cb043f2a7e9766bfa7c119e1b773441
Usuario <Usuario@JACAGU>
2023-03-07 17:49:26 +0100

    Primer commit holaMundo

---
+++ src/holamundo/MiPrimeraClase.java
@@ -0,0 +1,15 @@
+
+package holamundo;
+
+/**
+ *
+ * @author Juan Antonio Cabeza Guisado  
+ */
+
+public class MiPrimeraClase {
+    
+    public static void main(String args[]){
+        
+        System.out.println("Hola mundo!!");
+    }
+}
